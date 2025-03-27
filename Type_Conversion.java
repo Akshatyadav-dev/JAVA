554d9282ed93f56ce242b528a3c2c7fc6b42291c
Akshat Yadav <akshatyadav@Akshat-MacBook-Air.local>
2025-03-27 11:54:02 +0530

    second commit

--- Type_Conversion.java
+++ Type_Conversion.java
@@ -1,6 +1,15 @@
 public class Type_Conversion {
     public static void main(String[] args) {
-        
+        // Implicit conversion
+        int i = 100;
+        long l = i;  // No casting needed
+
+        // Explicit conversion
+        long l2 = 100L;
+        int i2 = (int) l2; // Casting required
+
+        System.out.println("Implicit conversion : " + l);
+        System.out.println("Explicit conversion : " + i2);
     }
     
 }
